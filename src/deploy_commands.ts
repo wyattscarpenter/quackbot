fe376a1e60371b4d0bb79ad68152fd48c78dfd61
keagud <keagud@protonmail.com>
2023-04-13 11:35:00 -0700

    add cli flag to deploy to global or testing server

--- src/deploy_commands.ts
+++ src/deploy_commands.ts
@@ -1,28 +1,38 @@
-import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import { REST, Routes } from "discord.js";
+import * as process from "node:process";
 import { secrets } from "./util";
 import { commands } from "./commands";
 
 const rest = new REST({ version: "10" }).setToken(secrets.token);
 const commands_json = [];
 
-for (let c of commands) {
+const global_deploy: boolean = process.argv[2]?.toLowerCase() === "global";
+
+for (const c of commands) {
   commands_json.push(c.data.toJSON());
 }
+
 (async () => {
   try {
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`
     );
 
-    const data = await rest.put(
-      Routes.applicationGuildCommands(secrets.clientId, secrets.guildId),
-      { body: commands_json }
-    );
+    if (global_deploy) {
+      console.log("Registering commands in the GLOBAL scope");
+      await rest.put(Routes.applicationCommands(secrets.clientId), {
+        body: commands_json,
+      });
+    } else {
+      console.log("Registering commands in the testing server only");
+
+      await rest.put(
+        Routes.applicationGuildCommands(secrets.clientId, secrets.guildId),
+        { body: commands_json }
+      );
+    }
     console.log(`Successfully reloaded ${commands.length}  commands.`);
   } catch (error) {
     console.error(error);
   }
 })();
-
-
-
