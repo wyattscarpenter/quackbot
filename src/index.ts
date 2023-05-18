34d5dd32923de0a33554b6908a820ae69ee9e10d
wyattscarpenter <wyattscarpenter@gmail.com>
2023-05-18 13:39:40 -0700

    functional command line

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,52 @@
 import { QuackClient, SlashCommand ,secrets } from "./util";
 import { commands } from "./commands";
 
-import {
-  Events,
-  REST,
-  GatewayIntentBits,
-  Collection,
-  Interaction,
-  CacheType,
-} from "discord.js";
+import { Events, REST, GatewayIntentBits, Collection, Interaction, CacheType, ChatInputCommandInteraction} from "discord.js";
 
 const client = new QuackClient({ intents: [GatewayIntentBits.Guilds] });
-if (secrets.token == "LOCAL") { //Local, non-discord route for testing and local use.
+const our_args = process.argv.slice(2);
+
+for (const c of commands) {
+  client.commands.set(c.data.name, c);
+}
+
+client.on(Events.InteractionCreate, (i: Interaction<CacheType>) => {
+    console.log(i);
+});
+
+client.on(
+  Events.InteractionCreate,
+  async (interaction: Interaction<CacheType>) => {
+    if (!interaction.isChatInputCommand()) {
+      return;
+    }
+    const subclient = interaction.client as QuackClient;
+
+    const command = subclient.commands.get(
+      interaction.commandName
+    ) as SlashCommand;
+
+    if (!command) {
+      console.error(`No command matching for ${interaction.commandName}`);
+      return;
+    }
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+
+client.once(Events.ClientReady, (c: any) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+
+// This is where we either login for real or just stay local. It must be after the other client-setup because if run with command-line arguments, we wish to immediately run a command, so that stuff must be defined.
+if (our_args[0]) { // [] is not actually falsy, so we have to check the first element to see if it exists or is undefined.
+  //run just one command
+  console.log(mock_command(our_args[0], ...our_args.slice(1)));
+} else if ( secrets.token == "LOCAL" ) { //Local, non-discord route for testing and local use.
   console.log("You are in local mode. This is a little CLI.");
   //This uses a disturbingly large amount of code for a simple cli...
   const readline = require('node:readline');
@@ -24,56 +59,38 @@ if (secrets.token == "LOCAL") { //Local, non-discord route for testing and local
   rl.prompt();
 
   rl.on('line', (line: string) => {
-    switch (line.trim()) {
-      case 'q':
-        rl.close();
-        break;
-      default:
-        console.log(`Ideally, I would try to run the command '${line.trim()}' from this, but I haven't implemented that logic yet.`);
-        break;
+    if (line.trim() == 'q'){
+      rl.close();
+    } else {
+      const line_split = line.split(" ");
+      mock_command(line_split[0], ...line_split.slice(1));
     }
     rl.prompt();
   }).on('close', () => {
-    process.exit(0);
+    process.exit(0); //This seems rather inelegant. Oh well.
   });
 } else { //Standard route; connects to discord using secret token
-  for (const c of commands) {
-    client.commands.set(c.data.name, c);
+  client.login(secrets.token);
+}
+
+async function mock_command(commandName: string, ...command_rest: string[]) {
+  const fake_user = {id: 1}
+  const interaction = {
+    isChatInputCommand: ()=> true, options: {getString: (name: string)=> command_rest.join(" ")||null}, reply: console.log
+  } as unknown as ChatInputCommandInteraction;
+
+  const command = client.commands.get(
+    commandName
+  ) as SlashCommand;
+
+  if (!command) {
+    console.error(`No command matching for ${commandName}`);
+    return;
   }
 
-  client.on(Events.InteractionCreate, (i: Interaction<CacheType>) => {
-    console.log(i);
-  });
-
-  client.on(
-    Events.InteractionCreate,
-    async (interaction: Interaction<CacheType>) => {
-      if (!interaction.isChatInputCommand()) {
-        return;
-      }
-
-      const subclient = interaction.client as QuackClient;
-
-      const command = subclient.commands.get(
-        interaction.commandName
-      ) as SlashCommand;
-
-      if (!command) {
-        console.error(`No command matching for ${interaction.commandName}`);
-        return;
-      }
-
-      try {
-        await command.execute(interaction);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  );
-
-  client.once(Events.ClientReady, (c: any) => {
-    console.log(`Logged in as ${c.user.tag}`);
-  });
-
-  client.login(secrets.token);
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
 }
